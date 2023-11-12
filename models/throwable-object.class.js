70026236da56821ade407bb0abab44362dfc44a0
Stefan Droste <stefan.droste@posteo.de>
2023-11-12 09:28:24 +0100

    Integrated bottle splash feature

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,35 +1,19 @@
 import { MovableObject } from './index.js';
 
 export class ThrowableObject extends MovableObject {
-
-    // IMAGES_BOTTLE_ROTATION = [
-    //     'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
-    //     'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
-    //     'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
-    //     'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
-    // ];
-
-    // IMAGES_BOTTLE_SPLASH = [
-    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
-    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
-    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
-    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
-    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
-    //     'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
-    // ];
-
     IMAGES_BOTTLE_ROTATION = Array.from({ length: 4 }, (_, index) => {
         return `img/6_salsa_bottle/bottle_rotation/${index + 1}_bottle_rotation.png`;
-      });
+    });
 
     IMAGES_BOTTLE_SPLASH = Array.from({ length: 6 }, (_, index) => {
         return `img/6_salsa_bottle/bottle_rotation/bottle_splash/${index + 1}_bottle_splash.png`;
-      });
+    });
 
     constructor(x, y) {
         super();
         this.loadImage('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
         this.loadImages(this.IMAGES_BOTTLE_ROTATION);
+        this.loadImages(this.IMAGES_BOTTLE_SPLASH);
         this.x = x;
         this.y = y;
         this.width = 60;
@@ -39,17 +23,32 @@ export class ThrowableObject extends MovableObject {
     }
 
     throw() {
-        this.speedY = 30;
+        this.speedY = 20;
+
         this.applyGravity();
-        setInterval(() => {   
-            this.x += 15;
-        }, 40)
+        this.intervalIds.push(
+            setInterval(() => {
+                if (this.speedY <= -45) {
+                    this.stopAnimate();
+                }
+                this.x += 15;
+            }, 40)
+        );
     }
 
-    animateBottle() {        
+    animateBottle() {
         setInterval(() => {
-            this.playAnimation(this.IMAGES_BOTTLE_ROTATION);
-            this.currentImage++;
+            if (this.hitEnemy) {
+                this.playAnimation(this.IMAGES_BOTTLE_SPLASH);
+                this.currentImage++;
+            } else {
+                this.playAnimation(this.IMAGES_BOTTLE_ROTATION);
+                this.currentImage++;
+            }
         }, 80);
+    }
+
+    stopAnimate() {
+        this.intervalIds.forEach(clearInterval);
     }
 }
